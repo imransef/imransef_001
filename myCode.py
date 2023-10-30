75acc6d190ac92e3f258d36291933f4ceeb1581a
Imran <131717628+imransef@users.noreply.github.com>
2023-10-30 15:37:19 +0300

    Update myCode.py

--- myCode.py
+++ myCode.py
@@ -1,1 +1,6 @@
-
+#this is a comment section because there is a hash key first
+#any comments are not printed on the monitor nor return anything
+NAME = 'Imran'
+age = '20'
+counTry = 'Ethiopian'
+uni_versity = 'Adiis Ababa University'
